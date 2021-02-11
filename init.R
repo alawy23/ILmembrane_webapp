da375c343d4b46959753905a6c881dcd79e6c62c
alawy23 <56697740+alawy23@users.noreply.github.com>
2021-02-10 19:29:08 -0600

    Add files via upload

--- init.R
+++ init.R
@@ -5,13 +5,5 @@
 # Example R code to install packages if not already installed
 #
 
-my_packages = c("shiny", "plotly", "stringr", "tidyverse", "shinyWidgets",
+helpers.installPackages("shiny", "plotly", "stringr", "tidyverse", "shinyWidgets",
                 "shinythemes", "htmlwidgets", "DT", "shinytoastr", "shinyhelper")
-
-install_if_missing = function(p) {
-  if (p %in% rownames(installed.packages()) == FALSE) {
-    install.packages(p)
-  }
-}
-
-invisible(sapply(my_packages, install_if_missing))
